ed3fee593cbb3dd9fbfd221bc4e16ead00add64c
Anna Jankowska <annie.jankowskiej@gmail.com>
2019-01-21 20:31:28 +0100

    MainWindow: create game board

--- Sapper/MainWindow.xaml.cs
+++ Sapper/MainWindow.xaml.cs
@@ -21,19 +21,68 @@ namespace Sapper
     public partial class MainWindow : Window
     {
         public const int MaxGridHeight = 20;
-        public const int MinGridHeight = 10;
+        public const int MinGridHeight = 7;
         public const int MaxGridLength = 20;
-        public const int MinGridLength = 10;
+        public const int MinGridLength = 7;
 
         public int GridHeight { get; set; }
         public int GridLength { get; set; }
 
+        public struct SquarePosition
+        {
+            public int X;
+            public int Y;
+            public SquarePosition(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
-            SizeToContent = SizeToContent.WidthAndHeight;
             var a = new SapperInstance();
             a.RevealSquare(5, 5);
+            InitializeVariables();
+            DrawBoard();
+        }
+
+        private void InitializeVariables()
+        {
+            GridLength = 12;
+            GridHeight = 12;
+        }
+
+        private void DrawBoard()
+        {
+            for (int j = 0; j < GridLength; j++)
+            {
+                Board.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < GridHeight; i++)
+            {
+                Board.RowDefinitions.Add(new RowDefinition());
+                for (int j = 0; j < GridLength; j++)
+                {
+                    var square = new Button();
+                    square.Style = Resources["UnrevealedField"] as Style;
+                    square.Tag = new SquarePosition(i, j);
+                    Board.Children.Add(square);
+                    Grid.SetRow(square, i);
+                    Grid.SetColumn(square, j);
+                }
+            }
+        }
+
+        private void Unrevealed_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Unrevealed_RightClick(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
